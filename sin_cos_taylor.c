eae39f8d72b38b5ec0960c1c9c3dfd2a9e798979
Albert Queraltó <48929187+albert-queralto@users.noreply.github.com>
2025-06-02 12:10:31 +0200

    Update sin_cos_taylor.c

--- sin_cos_taylor.c
+++ sin_cos_taylor.c
@@ -4,7 +4,7 @@
 // Helper function to compute factorial
 double factorial(int n) {
     double result = 1.0;
-    for (int i = 2; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
         result *= i;
     }
     return result;
